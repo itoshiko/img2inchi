b090ab50ab79c513a9fee190938b097b4ae4f715
itoshiko <tuhanz@163.com>
2021-05-12 23:11:48 +0800

    bug fix and further parameterization for data preparation

--- prepare.py
+++ prepare.py
@@ -10,7 +10,7 @@ from pkg.utils.general import Config
 @click.command()
 @click.option('--root', default="./",
               help="Path to your project root")
-@click.option('--pre_config', default="./config/data_prepare.yaml",
+@click.option('--pre_config', default="./config/data_prepare_small.yaml",
               help='Path to config yaml for data preparation')
 @click.option('--vocab_config', default="./config/vocab_small.yaml",
               help='Path to vocab yaml config')
@@ -24,24 +24,33 @@ def main(root, pre_config, vocab_config):
     SPLIT_DATA_SET = data_config.split_data_set
     BUILD_VOCAB = data_config.build_vocab
     PRC_IMG = data_config.prc_img
-    data_set = prc.read_data_set(root=root, dir_name=origin_dir, file_name='train_labels.csv')
+    train_labels = data_config.train_labels
+    val_set_labels = data_config.val_set_labels
+    train_set_labels = data_config.train_set_labels
+    img_width = data_config.img_width
+    img_height = data_config.img_height
+    threshold = data_config.threshold
+    num_threads = data_config.threads
+    data_set = prc.read_data_set(root=root, dir_name=origin_dir, file_name=train_labels)
     if BUILD_VOCAB:
         build_vocabulary(root=root, vocab_dir=vocab_dir, inchi_list=data_set['InChI'].values)
     if SPLIT_DATA_SET:
+        _config = {"val_set_labels": val_set_labels, "train_set_labels": train_set_labels}
         val_set, train_set = prc.train_val_split(root=root, dir_name=prcd_dir, data_set=data_set,
-                                                 train_size=TRAIN_SIZE, val_size=VAL_SIZE)
+                                                 train_size=TRAIN_SIZE, val_size=VAL_SIZE, config=_config)
     else:
-        val_set = prc.read_data_set(root=root, dir_name=prcd_dir, file_name='val_set_labels.csv')
-        train_set = prc.read_data_set(root=root, dir_name=prcd_dir, file_name='train_set_labels.csv')
+        val_set = prc.read_data_set(root=root, dir_name=prcd_dir, file_name=val_set_labels)
+        train_set = prc.read_data_set(root=root, dir_name=prcd_dir, file_name=train_set_labels)
     print(train_set.info())
     print(val_set.info())
     if PRC_IMG:
         train_img_list = train_set['image_id'].values
         val_img_list = val_set['image_id'].values
-        prc.prc_imgs(root=root, origin_dir_name=origin_dir, prcd_dir_name=prcd_dir,
-                     name='train', img_list=train_img_list, num_threads=8)
-        prc.prc_imgs(root=root, origin_dir_name=origin_dir, prcd_dir_name=prcd_dir,
-                     name='validate', img_list=val_img_list, num_threads=8)
+        __config = {"img_width": img_width, "img_height": img_height, "threshold": threshold}
+        prc.prc_imgs(config=__config, root=root, origin_dir_name=origin_dir, prcd_dir_name=prcd_dir,
+                     name='train', img_list=train_img_list, num_threads=num_threads)
+        prc.prc_imgs(config=__config, root=root, origin_dir_name=origin_dir, prcd_dir_name=prcd_dir,
+                     name='validate', img_list=val_img_list, num_threads=num_threads)
 
 
 if __name__ == "__main__":
