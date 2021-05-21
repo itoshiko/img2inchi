5387c88d342eacf4b029366f0248093dd83819e1
tfzx <2362595573@qq.com>
2021-05-21 14:22:36 +0800

    bugs fix

--- base.py
+++ base.py
@@ -15,7 +15,7 @@ class BaseModel(object):
 
     def _init_relative_path(self, output_dir):
         init_dir(output_dir)
-        training_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+        training_time = time.strftime("%Y-%m-%d %H.%M.%S", time.localtime())
         self._model_dir = output_dir + "/" + training_time
         init_dir(self._model_dir)
         self._model_path = self._model_dir + "/model.cpkt"
