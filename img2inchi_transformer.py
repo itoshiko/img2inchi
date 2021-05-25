e1b79a46e627ef3db0f9c6e4c20bc19421bf879f
tfzx <2362595573@qq.com>
2021-05-25 21:32:08 +0800

    upgrade features extractor

--- img2inchi_transformer.py
+++ img2inchi_transformer.py
@@ -3,7 +3,7 @@ import torch
 from img2inchi import Img2InchiModel
 from base import BaseModel
 from model.Transformer import Img2SeqTransformer
-from pkg.utils.utils import num_param
+from pkg.utils.utils import join, num_param
 
 PAD_ID = 0
 SOS_ID = 1
@@ -22,8 +22,9 @@ class Img2InchiTransformerModel(Img2InchiModel):
         else:
             feature_size = None
         extractor_name = self._config.transformer["extractor_name"]
-        max_seq_len = self.max_len
         tr_extractor = self._config.transformer["tr_extractor"]
+        pretrain = self._config.transformer['pretrain']
+        max_seq_len = self.max_len
         num_encoder_layers = self._config.transformer["num_encoder_layers"]
         num_decoder_layers = self._config.transformer["num_decoder_layers"]
         d_model = self._config.transformer["d_model"]
@@ -37,7 +38,7 @@ class Img2InchiTransformerModel(Img2InchiModel):
             dropout = self._config.transformer["dropout"]
         else:
             dropout = 0.1
-        model = Img2SeqTransformer(feature_size, extractor_name, max_seq_len, tr_extractor, num_encoder_layers,
+        model = Img2SeqTransformer(feature_size, extractor_name, pretrain, max_seq_len, tr_extractor, num_encoder_layers,
                                    num_decoder_layers, d_model, nhead, vocab_size, dim_feedforward, dropout)
         self.model = model
         print(f'The number of parameters: {num_param(model)}')
@@ -67,3 +68,22 @@ class Img2InchiTransformerModel(Img2InchiModel):
             return scheduler
         else:
             return super().getLearningRateScheduler(lr_scheduler)
+
+    def save(self):
+        """Saves model"""
+        self.logger.info("- Saving model...")
+        # save state as a dict
+        if self.multi_gpu:
+            model = self.model.module
+        else:
+            model = self.model
+        optimizer = self.optimizer
+        checking_point = {"net": model.state_dict(),
+                          "optimizer": optimizer.state_dict(),
+                          "epoch": self.now_epoch,
+                          "scheduler": self.scheduler.state_dict()}
+        if self._config.transformer["tr_extractor"]:
+            torch.save(model.features_extractor.extractor.state_dict(), self._model_path)
+        torch.save(checking_point, join(self._model_dir, 'pretrained_' + self._config.transformer['extractor_name'] + '.pth'))
+        self._config.save(self._config_export_path)
+        self.logger.info("- Saved model in {}".format(self._model_dir))
